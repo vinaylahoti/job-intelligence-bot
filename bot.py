cd57df9efacdf0b125a8088bb7cadfd4085cb8ca
vinaylahoti <135225089+vinaylahoti@users.noreply.github.com>
2026-02-15 22:23:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # Get values from environment variables (we'll set them later)
-BOT_TOKEN = os.getenv("8139550513:AAGPf_2n0qcogI4z7EAVtgJ_DuKUSmN3dVc")
-CHAT_ID = os.getenv("1053956354")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -19,3 +19,4 @@ if __name__ == "__main__":
     message = "✅ Job Intelligence Bot is live and working!"
     result = send_message(message)
     print(result)
+
